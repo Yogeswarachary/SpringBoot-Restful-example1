b9df8b0e6a77d87775ceb340fbb2550229d16a6a
Yogeswarachary <yogeswarachary.modepalli@skillassure.com>
2022-08-30 19:29:28 +0530

    first commit

---
+++ src/main/java/com/springbootrestful/SpringBootRestfulExample1Application.java
@@ -0,0 +1,13 @@
+package com.springbootrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulExample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootrestful/SpringBootRestfulExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
