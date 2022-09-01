e9cd3ab8b54e69812fa3c56c3bf828eb28f16611
Yogeswarachary <yogeswarachary.modepalli@skillassure.com>
2022-09-01 08:06:00 +0530

    Third commit with services and controllers

--- src/main/java/com/springbootrestful/SpringBootRestfulExample1Application.java
+++ src/main/java/com/springbootrestful/SpringBootRestfulExample1Application.java
@@ -1,9 +1,12 @@
 package com.springbootrestful;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
 public class SpringBootRestfulExample1Application {
 
 	public static void main(String[] args) {
